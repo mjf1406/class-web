94f6ea719f284561ef1eb3542b4e76b78252a3bb
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2025-01-28 16:15:37 +0900

    lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 /** @format */
 
-import FAQ from "@/components/landing/FAQ";
-import FeaturesSection from "@/components/landing/Features";
+// app/page.tsx
 import Hero from "@/components/landing/Hero";
-import PricingSection from "@/components/landing/Pricing";
+import dynamic from "next/dynamic";
+
+const FeaturesSection = dynamic(() => import("@/components/landing/Features"));
+const PricingSection = dynamic(() => import("@/components/landing/Pricing"));
+const FAQ = dynamic(() => import("@/components/landing/FAQ"));
 
 export default function Home() {
     return (
